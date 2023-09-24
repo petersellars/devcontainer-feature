9af3cd9c120110fce11ccc403405d17c30ba3207
Peter Sellars <psellars@gmail.com>
2023-09-24 05:41:22 +0000

    Git & Shellcheck in Devcontainer

--- test/adr-tools/test.sh
+++ test/adr-tools/test.sh
@@ -35,6 +35,7 @@ set -e
 # Optional: Import test library bundled with the devcontainer CLI
 # See https://github.com/devcontainers/cli/blob/HEAD/docs/features/test.md#dev-container-features-test-lib
 # Provides the 'check' and 'reportResults' commands.
+# shellcheck source=/dev/null
 source dev-container-features-test-lib
 
 check "adr-tools help" adr help
